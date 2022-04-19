97b8201ae4c3cfefa9c105c4c92d0fe91d3359da
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:47:48 +0300

    Soyutlama

---
+++ ConsoleAppSoyutlama/Myclass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSoyutlama
+{
+    abstract class Myclass
+    {
+        abstract class AbstractClass
+        {
+            public void deneme()
+            {
+                Console.WriteLine("Deneme");
+            }
+        }
+        class Abstract
+        {
+            class DerivedClass1 : AbstractClass
+            {
+
+            }
+            class DerivedClass2 : AbstractClass
+            {
+
+            }
+            
+        }
+    }
+}
